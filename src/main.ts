98cda8873d97da33d55c9215a3a6d1a20307c33d
Cai112233 <54765934+Wws54loi@users.noreply.github.com>
2022-02-10 13:46:48 +0800

    集成小程序能力

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import 'dsbridge'
 import router from './router'
 import piniaStore from './store'
 import './index.css'
+import './common/jssdk';
+
 // 支持SVG
 // import 'virtual:svg-icons-register'
 createApp(App).use(router).use(piniaStore).mount('#app')
--- src/pages/demo/test.vue
+++ src/pages/demo/test.vue
@@ -3,19 +3,22 @@
     Your content1223
     <div class="tset" style=""></div>
     <h1 @click="getUser">点击获取用户信息</h1>
-    <VantButton type="primary">主要按钮</VantButton>
+    <VantButton @click="toOtherPage" type="primary">主要按钮</VantButton>
   </div>
 </template>
 
 <script setup lang="ts" name="bin">
+  // const wx = window.wx
   import { Button as VantButton } from 'vant'
   import dsbridge from 'dsbridge'
   import { getUserInfo } from '@/common/hybridUtils'
-  console.log(dsbridge)
-
   dsbridge.hasNativeMethod('getNameSpace')
+   const wx = window.wx
   const getUser = () => {
     getUserInfo()
+  }
+  const toOtherPage = ()=>{
+    wx.miniProgram.navigateTo({ url:'/v3/pages/guide/assistant/index'})
   }
 </script>
 
